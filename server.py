e6d5506e015048c013dc1409ba9d73f3cb6f9fa3
Akshay Venugopal <kannanakshayvenugopal@gmail.com>
2017-07-09 16:54:42 +0530

    bug fix

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def server():
 	serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	serverSocket.setsockopt( socket.IPPROTO_TCP, socket.TCP_NODELAY, 1 )
 	#serverSocket.setsockopt( socket.SOL_SOCKET, socket.SO_SNDBUF, 100 )
-	host = "192.168.1.8"
+	host = "192.168.2.33"
 	port = 2345
 	serverSocket.bind( (host,port) )
 	try:
@@ -23,6 +23,8 @@ def server():
 				companyInterface.CompanyInterface( clientConnection, msgList ).start()
 			elif interfaceType == 'agencyinterface':
 				agencyInterface.AgencyInterface( clientConnection, msgList ).start()
+			else
+				clientConnection.close()
 	except:
 		print 'something went wrong : server'
 	finally:
